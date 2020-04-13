054dcb28ed1445a236b24f46da7273882b791b33
isaac <idgm@pm.me>
2020-04-13 16:26:47 -0500

    Fix lints for bad code example

--- examples/bad_examples/bubble_sort_bad.rb
+++ examples/bad_examples/bubble_sort_bad.rb
@@ -2,6 +2,9 @@
 # rubocop:disable Style/DocumentationMethod
 # rubocop:disable Style/MissingElse
 # rubocop:disable Style/MethodCallWithArgsParentheses
+# rubocop:disable Layout/TrailingWhitespace
+# rubocop:disable Layout/EmptyLines
+# rubocop:disable Layout/EmptyLineBetweenDefs
 
 def bubble_sort(array)
   array.length.times do
@@ -48,3 +51,6 @@ print bubble_sort([0, 5, 7, 2])
 # rubocop:enable Style/DocumentationMethod
 # rubocop:enable Style/MissingElse
 # rubocop:enable Style/MethodCallWithArgsParentheses
+# rubocop:enable Layout/TrailingWhitespace
+# rubocop:enable Layout/EmptyLines
+# rubocop:enable Layout/EmptyLineBetweenDefs
